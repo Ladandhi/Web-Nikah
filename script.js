a7b3057eb75c3297d217c2d656b5cf751bdf588c
Ladandhi <114547163+Ladandhi@users.noreply.github.com>
2026-06-27 23:23:30 +0700

    script.js

--- script.js
+++ script.js
@@ -2,8 +2,8 @@
 // ║           KONFIGURASI SUPABASE — ISI BAGIAN INI             ║
 // ║  Dapatkan dari: supabase.com → Project → Settings → API    ║
 // ╚══════════════════════════════════════════════════════════════╝
-const SUPABASE_URL = 'https://XXXXXXXX.supabase.co';  // ← ganti ini
-const SUPABASE_KEY = 'eyJXXXXXXXXXXXXXXXXXXXXXXXX';  // ← ganti ini (anon public key)
+const SUPABASE_URL = 'https://egvepeenlkqypurilygd.supabase.co/rest/v1/';  // ← ganti ini
+const SUPABASE_KEY = 'sb_publishable_1_ICZKca_uNHQ0ok-hn3SA_axndBLwO;  // ← ganti ini (anon public key)
 
 // ==========================================
 // HELPER: Kirim request ke Supabase REST API
